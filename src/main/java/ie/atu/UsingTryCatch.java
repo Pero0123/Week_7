289e47f09e5159de3351436385935eff0a9794ff
Pero0123 <g00422221@atu.ie>
2023-11-13 16:51:24 +0000

    improved loop condition

--- src/main/java/ie/atu/UsingTryCatch.java
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -4,10 +4,9 @@ import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
-        int check = 0;
         Scanner myscan = new Scanner(System.in);
         //runs until user enters a valid input
-        while (check == 0) {
+        while (true) {
             System.out.println("Please enter a number between 0 - 9:");
             try {
                 //read user input as a sting
@@ -19,7 +18,7 @@ public class UsingTryCatch {
                 //check if the number is between 0 and 9
                 if (myNum >= 0 && myNum <= 9) {
                     System.out.println("You entered " + myNum);
-                    check = 1;
+                    break;
                 } else {
                     System.out.println("Not a valid number");
                 }
